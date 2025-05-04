023d24a7d596a9e373123216e3da16628f22ef5c
ROHIT TIWARI <157032807+rohittiwari2225@users.noreply.github.com>
2025-05-05 00:45:43 +0530

    Add files via upload

---
+++ check_consonent_vowel.py
@@ -0,0 +1,14 @@
+a1=input("enter character to check  for vowel and consonent ")
+match  a1:
+    case 'a':
+        print('vowel')
+    case 'e':
+        print("vowel")
+    case 'i':
+        print("vowel")
+    case "o" :
+        print('vowel')   
+
+
+    case _ :
+        print("consonent")
